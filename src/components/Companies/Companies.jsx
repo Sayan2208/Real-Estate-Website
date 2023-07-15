220efc4b449b71a5f0f35145c90b149c95c86249
Sayan Datta <dattasayan825@gmail.com>
2023-07-15 16:21:20 +0530

    Initial Commit

---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Companies.css";
+
+function Companies() {
+  return (
+    <section className="c-wrapper">
+        <div className="paddings innerWidth flexCenter c-container">
+            <img src="/prologis.png" alt="Prologis Logo" />
+            <img src="/tower.png" alt="Tower Logo" />
+            <img src="/equinix.png" alt="Equinix Logo" />
+            <img src="/realty.png" alt="Realty Logo" />
+        </div>
+    </section>
+  )
+}
+
+export default Companies
